f9ae57ca10aaf1ee39136c390dfdfdb3729eb69a
Wouter Vos <voswouter87@hotmail.com>
2019-07-17 23:30:34 +0200

    Add Angular web project that will be served with docker containers and .NET Core.

---
+++ Yavalath/Yavalath/Engine/Board.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+using System.Text;
+
+namespace Engine
+{
+    public class Board
+    {
+		public const int a = 30, b = 20, c = 36;
+		public static readonly Point[] CornerDirections = new[] { new Point(a, b), new Point(c, 0), new Point(a, -b), new Point(-a, -b), new Point(-c, 0), new Point(-a, b) };
+
+		public static string GenerateHexagonalPoints(int x, int y)
+		{
+			var points = new StringBuilder("" + x + "," + y);
+
+			foreach(var c in CornerDirections)
+			{
+				x += c.X;
+				y += c.Y;
+				points.Append(" " + x + "," + y);
+			}
+
+			return points.ToString();
+		}
+    }
+}
