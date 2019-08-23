cc2d9812ea15cab9fd061706920fed34b6a4a280
VosWouter87 <voswouter87@hotmail.com>
2019-08-23 11:12:16 +0200

    Added work on set and cant stop to source.

---
+++ CantStop/CantStop/Column.cs
@@ -0,0 +1,17 @@
+﻿namespace CantStop
+{
+	public class Column
+	{
+		public int Target { get; }
+		public int Length { get; }
+		public bool Available { get; set; } = true;
+		public int[] Squares { get; }
+
+		public Column(int target, int length, int playerCount)
+		{
+			Target = target;
+			Length = length;
+			Squares = new int[playerCount];
+		}
+	}
+}
---
+++ CantStop/CantStop/Marker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CantStop
+{
+	public class Marker
+	{
+		public Column Column { get; private set; }
+		public int Position { get; private set; }
+
+		public void SetMarker(Column column, int start)
+		{
+			if (Column == null)
+			{
+				Column = column;
+				Position = start;
+			}
+			else throw new Exception("Clear the marker first.");
+		}
+
+		public int Stop()
+		{
+			var result = Position;
+			this.Abort();
+			return result;
+		}
+
+		public void Abort()
+		{
+			Column = null;
+			Position = 0;
+		}
+	}
+}
