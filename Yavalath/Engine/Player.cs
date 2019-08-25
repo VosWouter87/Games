b3ca06a8e651f5b748dad4536446d88dbaaccb74
Wouter Vos <voswouter87@hotmail.com>
2019-08-25 04:47:04 +0200

    Yavalath engine

---
+++ Yavalath/Engine/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Drawing;
+
+namespace Engine
+{
+	public class Player
+	{
+		public readonly Color Color;
+		public readonly bool IsWhite;
+
+		public Player(Color color, bool isWhite)
+		{
+			Color = color;
+			IsWhite = isWhite;
+		}
+	}
+}
